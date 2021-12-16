21d3dd8ab0f8c71968511f9558944f25c20a78f6
anubhavmonga <anubhavchitkara@gmail.com>
2021-12-16 22:21:17 +0530

    "final"

--- backend/server.js
+++ backend/server.js
@@ -11,27 +11,27 @@ import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 dotenv.config();
 
 connectDB();
-const path = require("path");
+
 const app = express();
-if (process.env.NODE_ENV === "development") {
-  app.use(morgan("dev"));
-}
-app.use(express.json());
+// if (process.env.NODE_ENV === "development") {
+//   app.use(morgan("dev"));
+// }
+// app.use(express.json());
 
-const __dirname = path.resolve();
-app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+// const __dirname = path.resolve();
+// app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/build")));
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "/frontend/build")));
 
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
-  );
-} else {
-  app.get("/", (req, res) => {
-    res.send("API is running....");
-  });
-}
+//   app.get("*", (req, res) =>
+//     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+//   );
+// } else {
+app.get("/", (req, res) => {
+  res.send("API is running....");
+});
+// }
 
 app.use("/api/data/", productRoutes);
 app.use("/api/users/", userRoutes);
@@ -45,17 +45,14 @@ app.get("/api/config/paypal", (req, res) =>
 // process.on("uncaughtException", () => console.log("hello"));
 // process.on("SIGTERM", () => console.log("hello"));
 
-// process.once("SIGUSR2", function () {
-//   process.kill(process.pid, "SIGUSR2");
-// });
+process.once("SIGUSR2", function () {
+  process.kill(process.pid, "SIGUSR2");
+});
 
-// process.on("SIGINT", function () {
-//   // this is only called on ctrl+c, not restart
-//   process.kill(process.pid, "SIGINT");
-// });
-
-app.use(notFound);
-app.use(errorHandler);
+process.on("SIGINT", function () {
+  // this is only called on ctrl+c, not restart
+  process.kill(process.pid, "SIGINT");
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`listening on port ${PORT}....`));
